c38c5ea4f414b1a3683b8c30736805dfae1b7c31
unknown <yaldaaghasi85@gmail.com>
2025-02-02 18:47:07 +0330

    nothing

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Resource , Api , reqparse ,fields , marshal_with , abort
 from datetime import datetime
+from flask_cors import CORS
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 api = Api(app) 
+CORS(app)
 class ArticleModel(db.Model):
     id = db.Column(db.Integer, primary_key=True) 
     title = db.Column(db.String(200), nullable=False)  
